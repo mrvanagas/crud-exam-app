4c2d0641aa407bf2a8d1d003dc510db8386a198d
mrvanagas <lukonelis1@gmail.com>
2021-03-31 12:14:30 +0300

    feat: preparing manager files

---
+++ client/src/Compononets/UserManager/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import API from '../../api';
+
+export default class CarsManager extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+};
