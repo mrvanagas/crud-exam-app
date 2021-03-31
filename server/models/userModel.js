f841ef06beb07487e87e56a6b88d77b4e7b3bd91
mrvanagas <lukonelis1@gmail.com>
2021-03-31 10:03:10 +0300

    feat: user schema created

---
+++ server/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    userAge: {
+        type: Number,
+        required: true
+    },
+    userEmail: {
+        type: String,
+        required: true
+    },
+    userPassword: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', schema);
